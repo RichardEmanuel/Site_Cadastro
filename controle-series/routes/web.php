b790c988a9f762c10a7b6e11a9e19ee3be7ea5b9
Richard Emanuel <ricardoepo444@gmail.com>
2022-02-01 22:58:47 -0300

    Rota de series e printando series

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -20,6 +20,22 @@ Route::get('/', function () {
 -> CRIANDO UMA ROTA COM O VERBO HTTP GET, PRECISO INFORMAR O CAMINHO(DENTRO DO GET), NO CASO QUAL A URL QUE VAI SER USADA neste caso foi o "/ola", o segundo parametro é uma função do php(no caso depois da virgula), e escolho oq vai sair na nossa função
 */
 
-Route::get('/ola', function(){
-    echo "Olá Mundo!";
+Route::get('/series', function(){
+    // arrey de series, inciriando as series
+    $series = [
+      'Supernatural',
+      'Ordem na casa - MARIE KONDO',
+      'Black Mirror'
+    ];
+    // Montando um html para printar as series
+
+    $html = "<ul>";
+    foreach($series as $serie){
+        $html .= "<li>$serie</li>";
+    }
+    $html .= "</ul>";
+
+    echo $html;
+    
+        
 });
