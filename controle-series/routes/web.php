e0a7df93be88e7eec5adf52ab92d3e838d6a74da
Richard Emanuel <ricardoepo444@gmail.com>
2022-02-05 17:19:00 -0300

    mod 2 data 05 02 2022

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -20,6 +20,12 @@ Route::get('/', function () {
 -> CRIANDO UMA ROTA COM O VERBO HTTP GET, PRECISO INFORMAR O CAMINHO(DENTRO DO GET), NO CASO QUAL A URL QUE VAI SER USADA neste caso foi o "/ola", o segundo parametro é uma função do php(no caso depois da virgula), e escolho oq vai sair na nossa função
 */
 
+/*MOD 2
+VAMOS APRENDER MVC -  padrão de arquitetura de software
+
+
+
+*/
 Route::get('/series', function(){
     // arrey de series, inciriando as series
     $series = [
