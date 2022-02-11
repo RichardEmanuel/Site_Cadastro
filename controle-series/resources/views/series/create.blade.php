483ee11b65e156c64536a851f4d139c71e5450fc
Richard Emanuel <ricardoepo444@gmail.com>
2022-02-11 12:11:34 -0300

    Ajustando butão de Adicionar da minha paginda de criar series

--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -11,11 +11,11 @@ Adicionar Séries do Ricardinho
         <div class="form-group mt-2">
             <label for="name" class="">Nome</label>
             <input type="text" class="form-control" name="nome" id="nome">
+            <div class="mt-2">
+                <button class="btn btn-primary">Adicionar</button>
+                <a href="/series" class="btn btn-dark">Voltar</a>
+            </div>
         </div>
     </form>
-    <div class="mt-2">
-        <button class="btn btn-primary">Adicionar</button>
-        <a href="/series" class="btn btn-dark">Voltar</a>
-    </div>
 
 @endsection
