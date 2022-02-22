9c28787cfde40b1bd668417c2188cffd1af8527f
Richard Emanuel <ricardoepo444@gmail.com>
2022-02-21 21:59:57 -0300

    Add butão

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -25,7 +25,11 @@ Séries do Ricardinho
                   onsubmit="return confirm('Tem certeza que deseja excluir {{addslashes($serie->nome)}} ?')">
                 @csrf
                 @method('DELETE')
-                <button class="btn btn-danger btn-sm">DELETE</button>
+                <button class="btn btn-danger btn-sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square-fill" viewBox="0 0 16 16">
+                        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z"/>
+                      </svg>
+                </button>
             </form>
         </li>
     @endforeach
