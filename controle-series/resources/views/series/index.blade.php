47f00aa73c9c544973576ce176cb2f0ca1c6602d
Richard Emanuel <ricardoepo444@gmail.com>
2022-02-23 21:12:16 -0300

    Iniciando Parte II

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -25,7 +25,12 @@ Séries do Ricardinho
                   onsubmit="return confirm('Tem certeza que deseja excluir {{addslashes($serie->nome)}} ?')">
                 @csrf
                 @method('DELETE')
-                <button class="btn btn-danger btn-sm">LIXO</button>
+                <button class="btn btn-danger btn-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+</svg>
+                </button>
             </form>
         </li>
     @endforeach
